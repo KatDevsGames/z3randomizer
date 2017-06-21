32708553a525eae2a4bcd800759c852e269a2fe0
Myramong <Myramong@users.noreply.github.com>
2017-06-21 13:36:29 -0300

    Vitreous & Trinexx rng hook fixes

--- hooks.asm
+++ hooks.asm
@@ -817,12 +817,12 @@ JSL.l RNG_Kholdstare
 org $1E97D5 ; <- F17D5 - sprite_kholdstare.asm : 605 (JSL GetRandomInt : AND.b #$04 : STA $0D)
 JSL.l RNG_Kholdstare
 ;--------------------------------------------------------------------------------
-org $1DE5B4 ; <- EE5E4 - sprite_vitreous.asm : 207 (JSL GetRandomInt : AND.b #$0F : TAY)
+org $1DE5E4 ; <- EE5E4 - sprite_vitreous.asm : 207 (JSL GetRandomInt : AND.b #$0F : TAY)
 JSL.l RNG_Vitreous
 org $1DE626 ; <- EE626 - sprite_vitreous.asm : 255 (JSL GetRandomInt : AND.b #$07 : STA $0D90, Y)
 JSL.l RNG_Vitreous
 ;--------------------------------------------------------------------------------
-org $1DB61C ; <- EB16C - sprite_trinexx.asm : 530 (JSL GetRandomInt : AND.b #$07 : TAY)
+org $1DB16C ; <- EB16C - sprite_trinexx.asm : 530 (JSL GetRandomInt : AND.b #$07 : TAY)
 JSL.l RNG_Trinexx
 org $1DB186 ; <- EB186 - sprite_trinexx.asm : 535 (JSL GetRandomInt : AND.b #$07 : TAY)
 JSL.l RNG_Trinexx
