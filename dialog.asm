c89604402cb7a19aa78c80549a45c9a9b57db8ff
sporchia <print@blindchaos.net>
2018-03-02 18:45:32 -0500

    use a different text routine to show triforce text

--- dialog.asm
+++ dialog.asm
@@ -261,7 +261,7 @@ RTL
 DialogTriforce:
 	%LoadDialogAddress(TriforceText)
     REP #$20 : LDA.w #$0171 : STA $1CF0 : SEP #$20 ; fix border
-	JSL.l Messaging_Text
+	JSL.l Main_ShowTextMessage
 RTL
 ;--------------------------------------------------------------------------------
 DialogGanon1:
