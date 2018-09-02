6dc8991b9986c9afac78a54ba114e7811285b3f1
Kevin Cathcart <kevincathcart@gmail.com>
2018-09-02 09:59:01 -0400

    Fix crystals lock for aga door
    Totally untested, may not even assemble.

--- entrances.asm
+++ entrances.asm
@@ -8,22 +8,21 @@ LockAgahnimDoors:
 		;#$0 = Never Locked
 		LDA.w #$0000 : RTL
 	+ : CMP.w #$0001 : BNE +
-		JSR.w OldLockAgahnimDoors : RTL
+		LDA $7EF3C5 : AND.w #$000F : CMP.w #$0002 : !BGE .unlock ; if we rescued zelda, skip
+			JSR.w LockAgahnimDoorsCore : RTL
 	+ : CMP.w #$0002 : BNE +
 		LDA $7EF37A : AND.w #$007F : CMP.w #$007F : BEQ .unlock
-			LDA.w #$0001
-			RTL
+			JSR.w LockAgahnimDoorsCore : RTL
 	+
 	.unlock
 	LDA.w #$0000 ; fallback to never locked
 RTL
 ;--------------------------------------------------------------------------------
-OldLockAgahnimDoors:
-	LDA $7EF3C5 : AND.w #$000F : CMP.w #$0002 : !BGE + ; if we rescued zelda, skip
-		LDA $22 : CMP.w #1992 : !BLT + ; door too far left, skip
-				  CMP.w #2088 : !BGE + ; door too rat right, skip
-		LDA $20 : CMP.w #1720 : !BGE + ; door too low, skip
-			LDA.w #$0001
+LockAgahnimDoorsCore:
+	LDA $22 : CMP.w #1992 : !BLT + ; door too far left, skip
+			  CMP.w #2088 : !BGE + ; door too rat right, skip
+	LDA $20 : CMP.w #1720 : !BGE + ; door too low, skip
+		LDA.w #$0001
 RTS
 	+
 	LDA.w #$0000
