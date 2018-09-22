0c395eb6456e820e9a8bf3f16afda0a56dc4c46e
sporchia <print@blindchaos.net>
2018-09-22 10:11:24 -0400

    fix spacing and retro arrow game money making

--- retro.asm
+++ retro.asm
@@ -34,16 +34,18 @@ DecrementArrows:
 	.infinite
 		LDA.b #$01 : RTL
 	.normal
-    	LDA $7EF377 : BEQ .done
-    	DEC : STA $7EF377 : INC
+		LDA $7EF377 : BEQ .done
+		DEC : STA $7EF377 : INC
 		BRA .done
 	.rupees
 		LDA $7EF340 : AND.b #$01 : BEQ +
+			LDA.b $0B99 : BNE + ; Arrow Game active and has credits left
+			LDA.b $0B9A : BNE + ; Arrow Game active and on last arrow
 			LDA.b #$00 : RTL
 		+
 		PHX
 		REP #$20 ; set 16-bit accumulator
-    	LDA $7EF360 : BEQ +
+		LDA $7EF360 : BEQ +
 			PHA : LDA $7EF340 : DEC : AND #$0002 : TAX : PLA
 			!SUB.l ArrowModeWoodArrowCost, X ; CMP.w #$0000
 			BMI .not_enough_money
@@ -58,16 +60,17 @@ DecrementArrows:
 RTL
 
 ArrowGame:
-    LDA $0B99 : BEQ +
-    	DEC $0B99 ; reduce minigame arrow count
+	LDA $0B99 : BEQ +
+		DEC $0B99 ; reduce minigame arrow count
 		LDA.l ArrowMode : BNE .rupees
 		.normal
-    		LDA $7EF377 : INC #2 : STA $7EF377 ; increment arrow count (by 2 for some reason)
+			LDA $7EF377 : INC #2 : STA $7EF377 ; increment arrow count (by 2 for some reason)
+			RTL
 		.rupees
 			PHX
 			REP #$20 ; set 16-bit accumulator
-				PHA : LDA $7EF340 : DEC : AND #$0002 : TAX : PLA
-		    	LDA $7EF360 : !ADD.l ArrowModeWoodArrowCost, X : STA $7EF360
+				LDA $7EF340 : DEC : AND #$0002 : TAX
+				LDA $7EF360 : !ADD.l ArrowModeWoodArrowCost, X : STA $7EF360
 			SEP #$20 ; set 8-bit accumulator
 			PLX
 	+
