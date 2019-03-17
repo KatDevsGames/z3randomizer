474af634408eb59eb8e4439011dd699649946e64
Kat <north@tinymagnet.com>
2019-03-16 22:28:54 -0400

    Update tables.asm

--- tables.asm
+++ tables.asm
@@ -1221,6 +1221,9 @@ db $04
 ;6A:Goal Item (Single/Triforce)
 ;6B:Goal Item (Multi/Power Star)
 
+;6C:Server Request Synchronous
+;6E:Server Request Asychronous
+
 ;DO NOT PLACE FREE DUNGEON ITEMS WITHIN THEIR OWN DUNGEONS - USE THE NORMAL VARIANTS
 
 ;70 - Map of Light World
