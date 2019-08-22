5a9368fbd5a59f042a8e69124aac8e565f3bde25
qwertymodo <qwertymodo@qwertymodo.com>
2019-08-21 19:02:01 -0700

    Typo

--- events.asm
+++ events.asm
@@ -203,7 +203,7 @@ RTL
 PostItemAnimation:
 	LDA.b #$00 : STA !ITEM_BUSY ; mark item as finished
 
-	LDA $7F50A9F : BEQ +
+	LDA $7F509F : BEQ +
 		STZ $1CF0 : STZ $1CF1 ; reset decompression buffer
 		JSL.l Main_ShowTextMessage_Alt
 		LDA.b #$00 : STA $7F509F
