5273112d9d80a26ab4993a3d275f4eb6fdee783f
spannerisms <32842036+spannerisms@users.noreply.github.com>
2021-03-24 08:13:41 -0400

    Update enemy_adjustments.asm
    this first since it can easily short circuit

--- enemy_adjustments.asm
+++ enemy_adjustments.asm
@@ -3,13 +3,14 @@
 ; make sure bats always load LW stats
 ;--------------------------------------------------------------------------------
 NewBatInit:
+	CPY #$00 : BEQ .light_world
 	;check if map id == 240 or 241
 	LDA $A0 : CMP #$F0 : BEQ .light_world ;oldman cave1
 	CMP #$F1 : BEQ .light_world ;oldman cave2
 	CMP #$B0 : BEQ .light_world ;agahnim statue keese
 	CMP #$D0 : BEQ .light_world ;agahnim darkmaze
 
-	CPY #$00 : BEQ .light_world
+	
 	LDA.b #$85 : STA $0CD2, X
 	LDA.b #$04 : STA $0E50, X
 RTL
