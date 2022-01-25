7150ceade97d5813f98a7a24fd1aaa444350d684
cassidoxa <cassidymoen@gmx.com>
2022-01-24 23:29:55 -0500

    Prevent potential lightning crash

--- zsnes.asm
+++ zsnes.asm
@@ -1,14 +1,17 @@
 ;--------------------------------------------------------------------------------
 CheckZSNES:
     SEP #$28
-    LDA #$FF
+    LDA.b #$FF
     CLC
-    ADC #$FF
-    CMP #$64
+    ADC.b #$FF
+    CMP.b #$64
     CLD
     BEQ .zsnes
+    LDA.l $02A3FE
+    CMP.b #ConditionalLightning&$FF
+    BNE .zsnes
     REP #$20
-    LDA #$01FF : TCS ; thing we wrote over - initialize stack
+    LDA.w #$01FF : TCS ; thing we wrote over - initialize stack
 JML.l ReturnCheckZSNES
 .zsnes
 	
