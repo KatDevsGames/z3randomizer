6e876b2d1fef81a7205dab96c025f3475273a889
cassidoxa <cassidymoen@gmx.com>
2022-12-08 23:16:20 -0500

    Update build scripts

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 del ..\working.sfc
 copy ..\alttp.sfc ..\working.sfc
-xkas.exe LTTP_RND_GeneralBugfixes.asm ..\working.sfc
+%~dp0bin\windows\asar.exe LTTP_RND_GeneralBugfixes.asm ..\working.sfc
 cmd /k
